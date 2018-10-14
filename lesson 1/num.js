a2e888e18d9b552d5212aa2ec137c5d8f536cf7a
Aleksandr <aleks.krais@mail.ru>
2018-10-14 14:34:08 +0700

    Добавил файлы в репозиторий

---
+++ lesson 1/num.js
@@ -0,0 +1,5 @@
+var num = 33721;
+var rez = 3*3*7*2*1;
+console.log(num);
+console.log(rez);
+console.log(Math.pow(rez, 3) );
---
+++ lesson 1/script.js
@@ -0,0 +1,26 @@
+var money = +prompt("Какой у Вас бюджет?");
+var shopName = prompt("Название Вашего магазина:");
+var mainList = {};
+var names = ['Иван', 'Жанна', 'Гоша']
+var employers = {
+ names
+};
+var shopGoods = prompt("Какой тип товара будем продавать?") + prompt("Какой тип товара будем продавать?") + prompt("Какой тип товара будем продавать?");
+
+
+mainList = {
+ money,
+ shopName,
+ shopGoods,
+ employers
+};
+
+console.log(mainList);
+console.log(money);
+console.log(shopName);
+console.log(shopGoods);
+console.log(employers);
+// console.log(typeof "shopGoods");
+
+alert("Ваш бюджет на масяц равен = " + money/30);
+
