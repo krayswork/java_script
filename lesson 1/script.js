9ff5907d6662d3046ba4559a3e83a15fc1d9ab31
Aleksandr <aleks.krais@mail.ru>
2018-10-14 15:22:27 +0700

    Переписал файл с программой

--- lesson 1/script.js
+++ lesson 1/script.js
@@ -1,26 +1,36 @@
-var money = +prompt("Какой у Вас бюджет?");
-var shopName = prompt("Название Вашего магазина:");
-var mainList = {};
-var names = ['Иван', 'Жанна', 'Гоша']
-var employers = {
- names
-};
-var shopGoods = prompt("Какой тип товара будем продавать?") + prompt("Какой тип товара будем продавать?") + prompt("Какой тип товара будем продавать?");
+let money = promot("Ваш бюджет?");
+let name = prompt("Название вашего магазина");
+let time = 19;
 
+let mainList = {
+ budget: money,
+ shopName: name,
+ shopGoods: [],
+ employers: {},
+ open: false
+}
 
-mainList = {
- money,
- shopName,
- shopGoods,
- employers
-};
+for (let i = 0; i < 5; i++){
+ let a = prompt("Какой тип товаров будем продавать?");
+ 
+ if ((typeof(a)) === 'string' && (typeof(a)) === null && a != '' && a.lenght < 50 ){
+  console.log('Все верно!')
+  mainList.shopGoods[i] = a;
+ } 
+}
+
+if (time < 0){
+ console.log('Не может быть!');
+} else if(time > 8 && time <20) {
+ console.log('Время работать');
+} else if(time < 24) {
+ console.log('Уже поздно');
+} else {
+ console.log('В сутках только 24 часа');
+}
+
+alert(mainList.budget / 30);
 
 console.log(mainList);
-console.log(money);
-console.log(shopName);
-console.log(shopGoods);
-console.log(employers);
-// console.log(typeof "shopGoods");
 
-alert("Ваш бюджет на масяц равен = " + money/30);
 
